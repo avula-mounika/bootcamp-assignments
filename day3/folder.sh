dc9df5eb3a05a7d5a416199c27fb0beae122a971
avula-mounika <87462461+avula-mounika@users.noreply.github.com>
2021-08-10 12:08:23 +0530

    Delete day3 directory

--- day3/folder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-echo -n "Enter the directory name:"
-read newdirname
-if [ -d "$newdirname" ]; then
-echo "Directory already exists" ;
-else
-`mkdir -p $newdirname`;
-echo "$newdirname directory is created"
-fi
